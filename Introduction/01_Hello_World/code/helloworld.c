6f4544b0e40d0055c48c41319e32f21466e81229
Andrew Miller <206alm@gmail.com>
2021-07-20 12:03:27 -0700

    add hello world

---
+++ Introduction/01_Hello_World/code/helloworld.c
@@ -0,0 +1,17 @@
+// include information about a standard library
+#include <stdio.h>
+// define a function called main, receives no parameters
+main()
+// open curlies
+{
+/*
+function statement
+the main library calls printf which returns a string
+\n attaches a new line to the end of the string
+*/
+	printf("hello world\n");
+	printf("hello, ");
+	printf("world");
+	printf("\c");
+// close curlies
+}
