eac7a070670367385e7930787dfc396aaf49a9f0
Andrew Miller <206alm@gmail.com>
2021-07-27 14:10:00 -0700

    corrected binary convertor

--- Types_Operators_Expressions/09_Bitwise_Operators/code/dectobin.c
+++ Types_Operators_Expressions/09_Bitwise_Operators/code/dectobin.c
@@ -5,13 +5,17 @@
 #include <ctype.h>
 
 #define MAX 32
+#define BIN_LEN 8
 
 void tobinary(int b[], int n);
 
 void main()
 {
-	int n;
-	int binary[10];
+	int n, i;
+	int binary[BIN_LEN];
+	for (i = 0; i < BIN_LEN; ++i) {
+		binary[i] = 0;
+	}
 	printf("Enter the number to convert: ");
 	scanf("%d",&n);
 	tobinary(binary, n);
@@ -19,16 +23,16 @@ void main()
 
 void tobinary(int binary[], int n)
 {
-	int i;
-	i = 0;
+	int i, j;
+	i = 2;
 
 	while (n > 0) {
 		binary[i] = n % 2;
 		n = n / 2;
 		++i;
 	}
-	for (i = i - 1; i >= 0; --i) {
-		printf("%d", binary[i]);
+	for (j = BIN_LEN - 1; j >= 0; --j) {
+		printf("%d", binary[j]);
 	}
 	printf("\n");
 }
